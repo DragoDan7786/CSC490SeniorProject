55417ea56a70abb3f4873cfe746c6924cedcbd13
jonathan-crain <124228802+jonathan-crain@users.noreply.github.com>
2023-09-30 17:03:33 -0400

    Add user_name to table. Add sample/test data insertion statement.

--- SQL/poc_connectivity.sql
+++ SQL/poc_connectivity.sql
@@ -11,7 +11,8 @@ GO
 --This table contains the values currently being stored in the User class.
 --The exception is hash: this is not currently being used, but will be used later if we are able to setup Firebase as our user registration service.
 CREATE TABLE poc_connectivity.[user](
-	userID INT IDENTITY(1,1)
+	userID INT IDENTITY(1,1) --will probably be replaced by the hash later, if we get that working
+	,[user_name] VARCHAR(250) NOT NULL
 	,[password] VARCHAR(250) NOT NULL
 	,first_name VARCHAR(250) NOT NULL
 	,middle_name VARCHAR(250)
@@ -27,5 +28,11 @@ CREATE TABLE poc_connectivity.[user](
 	,CONSTRAINT user_pk PRIMARY KEY(userID)
 );
 
+INSERT INTO poc_connectivity.[user]
+	([user_name], [password], first_name, middle_name, last_name, date_of_birth, street, city, [state], zip, phone_num, [hash], isAdmin)
+	VALUES
+	 ('admin','pass','Maddy','Em','Straightener','1973-12-11','123 Maple Street','Tree City', 'NY', '12345', '1-234-567-8901)','thisIsNotARealHash', 1)
+	,('user','pass','U.', 'S.','Er','1993-03-21','987 Grass Street', 'Plains City', 'WY', '98765', '9-876-543-2109','norIsThisARealHash', 0)
+;
 
 
