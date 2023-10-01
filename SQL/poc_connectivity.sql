7b128459a50af43382d50ecaf7f6da234cd4dd54
jonathan-crain <124228802+jonathan-crain@users.noreply.github.com>
2023-09-30 20:58:09 -0400

    Make all values NOT NULL, to ensure they will be present and thus simplify the logic for creating a User object.

--- SQL/poc_connectivity.sql
+++ SQL/poc_connectivity.sql
@@ -19,18 +19,18 @@ GO
 --The exception is hash: this is not currently being used, but will be used later if we are able to setup Firebase as our user registration service.
 CREATE TABLE poc_connectivity.[user](
 	userID INT IDENTITY(1,1) --will probably be replaced by the hash later, if we get that working
-	,userName VARCHAR(250) NOT NULL
+	,userName VARCHAR(250) NOT NULL UNIQUE
 	,pWord VARCHAR(250) NOT NULL
 	,firstName VARCHAR(250) NOT NULL
-	,middleName VARCHAR(250)
+	,middleName VARCHAR(250) NOT NULL
 	,lastName VARCHAR(250) NOT NULL
 	,dateOfBirth DATE NOT NULL
 	,street VARCHAR(250) NOT NULL
 	,city VARCHAR(250) NOT NULL
 	,[state] CHAR(2) NOT NULL
 	,zip CHAR(5) NOT NULL
-	,phoneNum VARCHAR(20)
-	,[hash] VARCHAR(250)
+	,phoneNum VARCHAR(20) NOT NULL
+	,[hash] VARCHAR(250) NOT NULL
 	,isAdmin BIT NOT NULL DEFAULT 0
 	,CONSTRAINT user_pk PRIMARY KEY(userID)
 );
@@ -39,7 +39,9 @@ INSERT INTO poc_connectivity.[user]
 	(userName, pWord, firstName, middleName, lastName, dateOfBirth, street, city, [state], zip, phoneNum, [hash], isAdmin)
 	VALUES
 	 ('admin','pass','Maddy','Em','Straightener','1973-12-11','123 Maple Street','Tree City', 'NY', '12345', '1-234-567-8901)','thisIsNotARealHash', 1)
-	,('user','pass','U.', 'S.','Er','1993-03-21','987 Grass Street', 'Plains City', 'WY', '98765', '9-876-543-2109','norIsThisARealHash', 0)
+	,('user','pass','Us', '','Er','1993-03-21','987 Grass Street', 'Plains City', 'WY', '98765', '9-876-543-2109','norIsThisARealHash', 0)
 ;
 
+SELECT * FROM poc_connectivity.[user];
 
+DROP TABLE poc_connectivity.[user];
