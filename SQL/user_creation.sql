1df397b5dd1ce12b60e51bbebdc89d5ba4c89c99
jonathan-crain <124228802+jonathan-crain@users.noreply.github.com>
2023-10-22 22:17:11 -0400

    Use case diagram created.

--- SQL/user_creation.sql
+++ SQL/user_creation.sql
@@ -4,3 +4,6 @@ GRANT SELECT ON SCHEMA::[poc_connectivity] TO csc490app;
 
 ALTER ROLE db_datawriter ADD MEMBER csc490app;
 ALTER ROLE db_datareader ADD MEMBER csc490app;
+
+CREATE USER suleman WITH PASSWORD = 'putPasswordHere';
+ALTER ROLE db_owner ADD MEMBER suleman;
