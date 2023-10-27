e22f4838e32c2eae2d4d8f3adea6a92710639d46
jonathan-crain <124228802+jonathan-crain@users.noreply.github.com>
2023-10-27 19:30:56 -0400

    Create query to retrieve all listings for a given user.

--- csc490buysellswap/src/main/java/com/suljo/csc490buysellswap/DbQueries.java
+++ csc490buysellswap/src/main/java/com/suljo/csc490buysellswap/DbQueries.java
@@ -2,29 +2,32 @@ package com.suljo.csc490buysellswap;
 
 public class DbQueries {
     public static String loginQuery = """
-               SELECT *
-               FROM sprint03.[user]
-               WHERE userName = ? AND pWord = ?;
-               """;
+        SELECT *
+        FROM sprint03.[user]
+        WHERE userName = ? AND pWord = ?;
+        """;
 
     public static String insertNewListingQuery = """
-            INSERT INTO sprint03.listing
-                (title, description, priceInCents, isForRent, rentalPeriodHours, sellerUserID, listingImage)
-            VALUES
-                (?,?,?,?,?,?,?)
-            ;
-            """;
+        INSERT INTO sprint03.listing
+            (title, description, priceInCents, isForRent, rentalPeriodHours, sellerUserID, listingImage)
+        VALUES
+            (?,?,?,?,?,?,?)
+        ;
+        """;
 
 
     public static String insertNewUserQuery = """
-            INSERT INTO sprint03.[user]
-                (userName, pWord, firstName, middleName, lastName, dateOfBirth, street, city, state, zip, phoneNum, isAdmin)
-            VALUES
-                (?,?,?,?,?,?,?,?,?,?,?,?)
-            ;
-      """;
+        INSERT INTO sprint03.[user]
+            (userName, pWord, firstName, middleName, lastName, dateOfBirth, street, city, state, zip, phoneNum, isAdmin)
+        VALUES
+            (?,?,?,?,?,?,?,?,?,?,?,?)
+        ;
+        """;
 
     public static String selectUserItemsQuery = """
-    
-    """;
+        SELECT listingID, title, description, priceInCents, isAvailable, isForRent, rentalPeriodHours, listingImage, sellerUserID, datetimeAdded, datetimeModified
+        FROM sprint03.listing
+        WHERE sellerUserID = ?
+        ;
+        """;
 }
