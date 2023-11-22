b07c085e42bc707949dcbe6fc2858cc073cb0261
jonathan-crain <124228802+jonathan-crain@users.noreply.github.com>
2023-11-21 22:05:37 -0500

    Update Message.java to use JavaFX Properties.

--- csc490buysellswap/src/main/java/com/suljo/csc490buysellswap/Message.java
+++ csc490buysellswap/src/main/java/com/suljo/csc490buysellswap/Message.java
@@ -1,80 +1,110 @@
 package com.suljo.csc490buysellswap;
 
+import javafx.beans.property.*;
+
 import java.time.LocalDateTime;
 
 public class Message {
-    private Integer messageID;
-    private String fromUsername;
-    private String toUsername;
-    private LocalDateTime datetimeSent;
-    private Integer aboutListingID;
-    private String subject;
-    private String contents;
+    private IntegerProperty messageID;
+    private StringProperty fromUsername;
+    private StringProperty toUsername;
+    private ObjectProperty<LocalDateTime> datetimeSent;
+    private IntegerProperty aboutListingID;
+    private StringProperty subject;
+    private StringProperty contents;
 
     public Message(Integer messageID, String fromUsername, String toUsername, LocalDateTime datetimeSent,
                    Integer aboutListingID, String subject, String contents) {
-        this.messageID = messageID;
-        this.fromUsername = fromUsername;
-        this.toUsername = toUsername;
-        this.datetimeSent = datetimeSent;
-        this.aboutListingID = aboutListingID;
-        this.subject = subject;
-        this.contents = contents;
+        this.messageID = new SimpleIntegerProperty(messageID);
+        this.fromUsername = new SimpleStringProperty(fromUsername);
+        this.toUsername = new SimpleStringProperty(toUsername);
+        this.datetimeSent = new SimpleObjectProperty<>(datetimeSent);
+        this.aboutListingID = new SimpleIntegerProperty(aboutListingID);
+        this.subject = new SimpleStringProperty(subject);
+        this.contents = new SimpleStringProperty(contents);
     }
 
     public Integer getMessageID() {
-        return messageID;
+        return messageID.get();
     }
 
     public void setMessageID(Integer messageID) {
-        this.messageID = messageID;
+        this.messageID.set(messageID);
+    }
+
+    public IntegerProperty messageIDProperty(){
+        return messageID;
     }
 
     public String getFromUsername() {
-        return fromUsername;
+        return fromUsername.get();
     }
 
     public void setFromUsername(String fromUsername) {
-        this.fromUsername = fromUsername;
+        this.fromUsername.set(fromUsername);
+    }
+
+    public StringProperty fromUsernameProperty(){
+        return fromUsername;
     }
 
     public String getToUsername() {
-        return toUsername;
+        return toUsername.get();
     }
 
     public void setToUsername(String toUsername) {
-        this.toUsername = toUsername;
+        this.toUsername.set(toUsername);
+    }
+
+    public StringProperty toUsernameProperty(){
+        return toUsername;
     }
 
     public LocalDateTime getDatetimeSent() {
-        return datetimeSent;
+        return datetimeSent.get();
     }
 
     public void setDatetimeSent(LocalDateTime datetimeSent) {
-        this.datetimeSent = datetimeSent;
+        this.datetimeSent.set(datetimeSent);
+    }
+
+    public ObjectProperty<LocalDateTime> datetimeSentProperty(){
+        return datetimeSent;
     }
 
     public Integer getAboutListingID() {
-        return aboutListingID;
+        return aboutListingID.get();
     }
 
     public void setAboutListingID(Integer aboutListingID) {
-        this.aboutListingID = aboutListingID;
+        this.aboutListingID.set(aboutListingID);
+    }
+
+    public IntegerProperty aboutListingIDProperty(){
+        return aboutListingID;
     }
 
     public String getSubject() {
-        return subject;
+        return subject.get();
     }
 
     public void setSubject(String subject) {
-        this.subject = subject;
+        this.subject.set(subject);
+    }
+
+    public StringProperty subjectProperty(){
+        return subject;
     }
 
     public String getContents() {
-        return contents;
+        return contents.get();
     }
 
     public void setContents(String contents) {
-        this.contents = contents;
+        this.contents.set(contents);
+    }
+
+    public StringProperty contentsProperty() {
+        return contents;
     }
 }
